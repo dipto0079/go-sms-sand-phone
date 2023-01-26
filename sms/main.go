c27ad4305ba13f628216bb95ff35fde5193e4aee
DiptoCodemen <sudipto@codemen.net>
2023-01-26 17:27:21 +0600

    update

--- sms/main.go
+++ sms/main.go
@@ -7,22 +7,22 @@ import (
 	twilioApi "github.com/twilio/twilio-go/rest/api/v2010"
 )
 
-func SendSms(accountSid string, authToken string,setTo string,setFrom string,yourSms string) (resp *twilioApi.ApiV2010Message, err error,response string) {
+func SendSms(accountSid, authToken, to, from, message string) (resp *twilioApi.ApiV2010Message, err error, response string) {
 	client := twilio.NewRestClientWithParams(twilio.ClientParams{
 		Username: accountSid,
 		Password: authToken,
 	})
 
 	params := &twilioApi.CreateMessageParams{}
-	params.SetTo(setTo)
-	params.SetFrom(setFrom)
-	params.SetBody(yourSms)
+	params.SetTo(to)
+	params.SetFrom(from)
+	params.SetBody(message)
 
 	resp, err = client.Api.CreateMessage(params)
 	if err != nil {
-		return resp, err,"message"
+		return resp, err, "message"
 	} else {
 		response, _ := json.Marshal(*resp)
-		return resp, err,string(response)
+		return resp, err, string(response)
 	}
 }
